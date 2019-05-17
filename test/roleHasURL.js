3d65391856b2a484a66b2d72c08a63d6bf83f653
antonio <afontaine@uc.cl>
2019-05-17 00:02:52 -0400

    url matches tests done

---
+++ test/roleHasURL.js
@@ -0,0 +1,34 @@
+process.env.NODE_ENV = 'test';
+
+const { assert } = require('chai');
+const main = require('../main');
+
+
+describe('role has URL', () => { // eslint-disable-line no-undef, max-lines-per-function
+
+  before(() => { // eslint-disable-line no-undef
+    main.flushAccessList();
+    main.setAccess({
+      role: ['/other', '/path/to/yes'],
+    });
+  });
+
+
+  it('it has the path', (done) => { // eslint-disable-line no-undef
+    assert.isTrue(main.roleHasURL('role', '/path/to/yes'));
+    done();
+  });
+
+  it('it does not have the path', (done) => { // eslint-disable-line no-undef
+    assert.isFalse(main.roleHasURL('role', '/path/to/no'));
+
+    done();
+  });
+
+
+  it('role not present in access', (done) => { // eslint-disable-line no-undef
+    assert.isFalse(main.roleHasURL('other', '/path/to/no'));
+    done();
+  });
+
+});
