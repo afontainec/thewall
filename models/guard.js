70b636d698c7edb65d8fad1667011c1bd1371443
antonio <afontaine@uc.cl>
2019-06-04 11:42:25 -0400

    test passing for extract filter

--- models/guard.js
+++ models/guard.js
@@ -1,14 +1,14 @@
 const hasAccess = (userId, url, entries) => {
   if (isEmpty(entries)) return true; // if its not restricted then its open to everyone
-  const validCombinations = parseEntries(url, entries);
+  const validCombinations = getCombinations(url, entries);
   // return DatabaseManager.hasAccess(userId, validCombinations);
 };
 
 function isEmpty(json) {
-  return Object.keys(json) === 0;
+  return !json || Object.keys(json) === 0;
 }
 
-const parseEntries = (url, entries) => {
+const getCombinations = (url, entries) => {
   const roles = Object.keys(entries);
   const array = [];
   for (let i = 0; i < roles.length; i++) {
@@ -22,20 +22,20 @@ const buildCombination = (url, role, entry) => {
   if (!role || !entry) return [];
   const combination = [role];
   if (!entry.filter) return combination;
-  combination.push(extractFilter(url, entry.path, entry.filter));
-  console.log('--------------------------------');
-  console.log(entry);
-  console.log(combination);
-  console.log('--------------------------------');
+  const filterValue = extractFilter(url, entry.path, entry.filter);
+  combination.push(filterValue);
   return combination;
 };
 
 const extractFilter = (url, templateUrl, filter) => {
+  if (!url || !templateUrl) return null;
   const divider = `:${filter}`;
   const parts = templateUrl.split(divider);
+  if (parts.length !== 2) return null;
   const extractor = `^${parts[0]}(.*)${parts[1]}$`;
   const regex = new RegExp(extractor);
-  return url.match(regex)[1];
+  const result = url.match(regex);
+  return result && result[1] ? result[1] : null;
 };
 
 
--- test/guard/extractFilter.js
+++ test/guard/extractFilter.js
@@ -13,19 +13,33 @@ describe('Extract Filter', () => { // eslint-disable-line no-undef, max-lines-pe
   });
 
   it('url is null', (done) => { // eslint-disable-line no-undef
-    done('NOT IMPLEMENTED');
+    const filtering = Guard.extractFilter(null, '/places/:id/some', 'id');
+    assert.equal(filtering, null);
+    done();
   });
 
   it('templateUrl is null', (done) => { // eslint-disable-line no-undef
-    done('NOT IMPLEMENTED');
+    const filtering = Guard.extractFilter('/places/3/some', null, 'id');
+    assert.equal(filtering, null);
+    done();
   });
 
   it('templateUrl ends with filter', (done) => { // eslint-disable-line no-undef
-    done('NOT IMPLEMENTED');
+    const filtering = Guard.extractFilter('/places/3', '/places/:id', 'id');
+    assert.equal(filtering, '3');
+    done();
   });
 
   it('templateUrl does not contain filter', (done) => { // eslint-disable-line no-undef
-    done('NOT IMPLEMENTED');
+    const filtering = Guard.extractFilter('/places/3', '/places/id', 'id');
+    assert.equal(filtering, null);
+    done();
+  });
+
+  it('url is different to templateUrl', (done) => { // eslint-disable-line no-undef
+    const filtering = Guard.extractFilter('/other/3', '/places/:id', 'id');
+    assert.equal(filtering, null);
+    done();
   });
 
 
