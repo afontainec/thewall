c27bbe6cef952354221011735d44b7ebafab6c69
afontainec <afontaine@uc.cl>
2021-03-16 19:28:32 -0300

    feat(databasemanager): pass to class

--- models/DatabaseManager.js
+++ models/DatabaseManager.js
@@ -1,79 +1,53 @@
-const DEFAULT_NAME = 'thewall_access';
-let knex;
 
 
-// INITIALIZE
+class DatabaseManager {
+  constructor(config) {
+    this.DEFAULT_NAME = 'thewall_access';
 
-const init = async (config) => {
-  setKnex(config);
-  return createTable();
-  // const exists = await isTableCreated();
-  // return exists ? true : createTable();
-};
+    this.setKnex(config);
+    this.createTable().then().catch(() => {});
+  }
 
-const createTable = async () => {
-  const query = knex.raw(`CREATE TABLE IF NOT EXISTS ${DEFAULT_NAME} (id SERIAL, user_id INTEGER, role TEXT, filter TEXT)`);
-  return query;
-};
+  async createTable() {
+    const query = this.knex.raw(`CREATE TABLE IF NOT EXISTS ${this.DEFAULT_NAME} (id SERIAL, user_id INTEGER, role TEXT, filter TEXT)`);
+    return query;
+  }
 
-// const isTableCreated = async () => {
-//   const query = knex.select('*').from('information_schema.tables').where('table_name', DEFAULT_NAME);
-//   const results = await query;
-//   return results.length > 0;
-// };
+  getKnex() {
+    return this.knex;
+  }
 
-const getKnex = () => {
-  return knex;
-};
+  setKnex(config) {
+    if (!config || !config.knex) throw new Error('No knex set. A given knex must be given');
+    this.knex = require(config.knex); // eslint-disable-line import/no-dynamic-require, global-require
+  }
 
-const setKnex = (config) => {
-  if (!config || !config.knex) throw new Error('No knex set. A given knex must be given');
-  knex = require(config.knex); // eslint-disable-line import/no-dynamic-require, global-require
-};
+  table() {
+    return this.knex(this.DEFAULT_NAME);
+  }
 
-const table = () => {
-  return knex(DEFAULT_NAME);
-};
+  // CRUD ACCESS
 
-// CRUD ACCESS
+  async addAccess(access) {
+    return access ? this.knex(this.DEFAULT_NAME).insert(access) : true;
+  }
 
-const addAccess = async (access) => {
-  return access ? knex(DEFAULT_NAME).insert(access) : true;
-};
+  async deleteAccess(access) {
+    return access ? this.knex(this.DEFAULT_NAME).del().where(access) : true;
+  }
 
-const deleteAccess = async (access) => {
-  return access ? knex(DEFAULT_NAME).del().where(access) : true;
-};
+  async findAccess(access) {
+    return access ? this.knex(this.DEFAULT_NAME).select('*').where(access) : [];
+  }
 
-const findAccess = async (access) => {
-  return access ? knex(DEFAULT_NAME).select('*').where(access) : [];
-};
+  async updateAccess(where, newValues) {
+    return where ? this.knex(this.DEFAULT_NAME).update(newValues).where(where) : [];
+  }
 
-const updateAccess = async (where, newValues) => {
-  return where ? knex(DEFAULT_NAME).update(newValues).where(where) : [];
-};
-
-const flushAccess = async () => {
-  return knex(DEFAULT_NAME).del();
-};
-
-
-const publicMethods = {
-  init,
-  addAccess,
-  deleteAccess,
-  findAccess,
-  flushAccess,
-  updateAccess,
-  table,
-};
-
-if (process.env.NODE_ENV === 'test') {
-  publicMethods.createTable = createTable;
-  publicMethods.DEFAULT_NAME = DEFAULT_NAME;
-  // publicMethods.isTableCreated = isTableCreated;
-  publicMethods.getKnex = getKnex;
-  publicMethods.setKnex = setKnex;
+  async flushAccess() {
+    return this.knex(this.DEFAULT_NAME).del();
+  }
 }
 
-module.exports = publicMethods;
+
+module.exports = DatabaseManager;
