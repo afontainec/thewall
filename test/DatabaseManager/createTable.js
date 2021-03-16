eb9d38a4e9882573e6d46116a91025fe72ee446f
afontainec <afontaine@uc.cl>
2021-03-16 19:28:51 -0300

    fix(databasemanager): tests

--- test/DatabaseManager/createTable.js
+++ test/DatabaseManager/createTable.js
@@ -4,25 +4,27 @@ const { assert } = require('chai');
 const config = require('../../config');
 const DatabaseManager = require('../../models/DatabaseManager');
 
+let databaseManager;
 
 describe('created', () => { // eslint-disable-line no-undef, max-lines-per-function
 
   before(async () => { // eslint-disable-line no-undef
-    DatabaseManager.setKnex(config);
-    const results = await DatabaseManager.getKnex().raw(`SELECT * FROM information_schema.tables WHERE  table_name = '${DatabaseManager.DEFAULT_NAME}'`);
-    if (results.rows.length > 0) await DatabaseManager.getKnex().raw(`DROP TABLE ${DatabaseManager.DEFAULT_NAME}`);
+    databaseManager = await new DatabaseManager(config);
+    databaseManager.setKnex(config);
+    const results = await databaseManager.getKnex().raw(`SELECT * FROM information_schema.tables WHERE  table_name = '${databaseManager.DEFAULT_NAME}'`);
+    if (results.rows.length > 0) await databaseManager.getKnex().raw(`DROP TABLE ${databaseManager.DEFAULT_NAME}`);
   });
 
   it('it is not created', async () => { // eslint-disable-line no-undef
-    await DatabaseManager.createTable();
-    const results = await DatabaseManager.getKnex().raw(`SELECT * FROM information_schema.tables WHERE  table_name = '${DatabaseManager.DEFAULT_NAME}'`);
+    await databaseManager.createTable();
+    const results = await databaseManager.getKnex().raw(`SELECT * FROM information_schema.tables WHERE  table_name = '${databaseManager.DEFAULT_NAME}'`);
     assert.equal(results.rows.length, 1);
   });
 
   it('it is created', async () => { // eslint-disable-line no-undef
-    await DatabaseManager.table().insert({ user_id: 1 });
-    await DatabaseManager.createTable();
-    const results = await DatabaseManager.table().count('*');
+    await databaseManager.table().insert({ user_id: 1 });
+    await databaseManager.createTable();
+    const results = await databaseManager.table().count('*');
     assert.equal(results[0].count, 1);
   });
 
--- test/DatabaseManager/init.js
+++ test/DatabaseManager/init.js
@@ -4,24 +4,38 @@ const { assert } = require('chai');
 const config = require('../../config');
 const DatabaseManager = require('../../models/DatabaseManager');
 
+let databaseManager;
+
 
 describe('init', () => { // eslint-disable-line no-undef, max-lines-per-function
 
   before(async () => { // eslint-disable-line no-undef
-    DatabaseManager.setKnex(config);
-    await DatabaseManager.getKnex().raw(`DROP TABLE IF EXISTS ${DatabaseManager.DEFAULT_NAME}`);
+    databaseManager = await new DatabaseManager(config);
+    databaseManager.setKnex(config);
+    await databaseManager.getKnex().raw(`DROP TABLE IF EXISTS ${databaseManager.DEFAULT_NAME}`);
   });
 
   it('table it is not created', async () => { // eslint-disable-line no-undef
-    await DatabaseManager.init(config);
-    const results = await DatabaseManager.getKnex().raw(`SELECT * FROM information_schema.tables WHERE  table_name = '${DatabaseManager.DEFAULT_NAME}'`);
+    databaseManager = await new DatabaseManager(config);
+    await wait(100);
+    const results = await databaseManager.getKnex().raw(`SELECT * FROM information_schema.tables WHERE  table_name = '${databaseManager.DEFAULT_NAME}'`);
     assert.equal(results.rows.length, 1);
   });
 
   it('it is created', async () => { // eslint-disable-line no-undef
-    await DatabaseManager.init(config);
-    const results = await DatabaseManager.getKnex().raw(`SELECT * FROM information_schema.tables WHERE  table_name = '${DatabaseManager.DEFAULT_NAME}'`);
+    databaseManager = await new DatabaseManager(config);
+    await wait(100);
+    const results = await databaseManager.getKnex().raw(`SELECT * FROM information_schema.tables WHERE  table_name = '${databaseManager.DEFAULT_NAME}'`);
     assert.equal(results.rows.length, 1);
   });
 
 });
+
+
+const wait = function waitFor(ms) {
+  return new Promise(((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  }));
+};
